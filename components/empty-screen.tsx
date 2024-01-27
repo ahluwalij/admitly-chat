f650691dfffd0bcf3afe7c992fd2610572f4fff9
ahluwalij <jaspay6@gmail.com>
2024-01-26 22:00:26 -0500

    fe customization

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -3,31 +3,59 @@ import { UseChatHelpers } from 'ai/react'
 import { Button } from '@/components/ui/button'
 import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
-
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'College Application Process',
+    message: 'Can you explain the steps involved in the University of Maryland, College Park application process?'
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'College Preparation Timeline',
+    message: 'What should I be doing in my junior year of high school to prepare for college?'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
-  }
+    heading: 'Life on Campus',
+    message: 'What is campus life like at the University of Maryland, College Park?'
+  },
+  {
+    heading: 'Paying for College',
+    message: 'How do I apply for scholarships and financial aid at the University of Maryland, College Park?'
+  },
+  {
+    heading: 'Choosing a Major',
+    message: 'How do I choose the right major for me at the University of Maryland, College Park?'
+  },
+  // {
+  //   heading: 'College Credits',
+  //   message: 'What are college credits at the University of Maryland, College Park and how do they work?'
+  // },
+  // {
+  //   heading: 'Tips for Success',
+  //   message: 'Do you have any tips for succeeding academically at the University of Maryland, College Park?'
+  // },
+  // {
+  //   heading: 'Work-Study Balance',
+  //   message: 'How can I balance a part-time job with my studies at the University of Maryland, College Park?'
+  // },
+  {
+    heading: 'Extracurriculars in College',
+    message: 'What kind of extracurricular activities can I get involved in at the University of Maryland, College Park?'
+  },
+  // {
+  //   heading: 'Dealing with Homesickness',
+  //   message: 'How do students typically deal with homesickness at the University of Maryland, College Park?'
+  // }
 ]
+
 
 export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to Admitly AI Chatbot!
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
+          This is an AI chatbot built to answer any questions you may have about applying to UMD. This is built with{' '}
           <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
           <ExternalLink href="https://vercel.com/storage/kv">
             Vercel KV
